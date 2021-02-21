ab5e3fdc9d240a3af9723670e0202fa0f7a4b327
Paul Staroch <paulchen@rueckgr.at>
2021-02-21 22:59:14 +0100

    introduce application and distribution plugins for Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.30"
     `java-library`
+    application
+    distribution
 }
 
 repositories {
@@ -16,4 +18,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClass.set("at.rueckgr.spotify.main.DiscoverWeeklyClonerKt")
+}
+
+distributions {
+    main
 }
