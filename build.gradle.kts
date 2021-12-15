7846fad0bbfe74126b8727bdc222de885c17f20f
paulchen <paulchen@rueckgr.at>
2021-12-15 01:03:49 +0100

    update log4j to 2.16.0 (CVE-2021-45046)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ repositories {
 
 dependencies {
     api("se.michaelthelin.spotify:spotify-web-api-java:6.5.2")
-    api("org.apache.logging.log4j:log4j-api:2.15.0")
+    api("org.apache.logging.log4j:log4j-api:2.16.0")
     api("org.apache.logging.log4j:log4j-api-kotlin:1.0.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.15.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.15.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.16.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.16.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
