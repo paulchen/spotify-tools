5a31f700b9b7cdb39973c8b0099a5b25987c80e5
Paul Staroch <paulchen@rueckgr.at>
2022-11-09 22:07:21 +0100

    fix logging dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     api("org.apache.logging.log4j:log4j-api:2.19.0")
     api("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
     implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.18.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
