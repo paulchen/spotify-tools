8e65ebffd0756e8ebdcdcd65957125c96bc7ff83
Paul Staroch <paulchen@rueckgr.at>
2024-12-04 18:01:47 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
     `java-library`
     application
     distribution
-    id("com.github.ben-manes.versions") version "0.43.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 repositories {
@@ -11,11 +13,11 @@ repositories {
 }
 
 dependencies {
-    api("se.michaelthelin.spotify:spotify-web-api-java:7.2.2")
-    api("org.apache.logging.log4j:log4j-api:2.19.0")
-    api("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
+    api("se.michaelthelin.spotify:spotify-web-api-java:8.4.1")
+    api("org.apache.logging.log4j:log4j-api:2.24.2")
+    api("org.apache.logging.log4j:log4j-api-kotlin:1.5.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.2")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.2")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
